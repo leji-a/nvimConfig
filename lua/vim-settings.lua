937e64607a48a2172a6ef58912d260b76facaa13
leji-a <toviya3135@gmail.com>
2025-03-28 23:46:34 -0300

    numbers plugin removed. relative numbers will appear always.

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.wo.relativenumber = true
 vim.g.mapleader = " "
 vim.loader.enable()
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
