482121e8434a3986ffbd252721845f5ccd505e8f
leji-a <toviya3135@gmail.com>
2025-03-29 22:21:11 -0300

    mason_tool_installer added.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,10 +2,12 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup({
 			ui = {
@@ -19,6 +21,13 @@ return {
 		mason_lspconfig.setup({
 			ensure_installed = {
 				"lua_ls",
+			},
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua",
+				"black",
+				"isort",
 			},
 		})
 	end,
