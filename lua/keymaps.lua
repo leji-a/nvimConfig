85ace5fb56ceab8a420ade4dc5fe3b835918dc86
leji-a <toviya3135@gmail.com>
2025-10-21 13:32:19 -0300

    keybind added.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,5 @@ local keymap = vim.keymap.set
 keymap("n", "<leader>s", ":Ex<CR>", { desc = "Open Netrw" })
 
 keymap("n", "<leader>lz", ":Lazy<CR>", { desc = "Open lazy menu" })
+
+keymap("n", "<leader>ms", ":Mason<CR>", { desc = "Open mason menu" })
