702bc74dc46e42e090f2b1b4af2d2b6c7b71e1b1
leji-a <toviya3135@gmail.com>
2026-04-11 23:34:27 -0300

    hehe

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,14 +7,11 @@ return {
 				local opts = { noremap = true, silent = true, buffer = args.buf }
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 			end,
 		})
-
-		local servers = { "rust_analyzer", "clangd", "zls" }
-		for _, server in ipairs(servers) do
-			vim.lsp.config(server, {})
-			vim.lsp.enable(server)
-		end
 
 		vim.lsp.config("lua_ls", {
 			settings = {
@@ -24,13 +21,15 @@ return {
 						checkThirdParty = false,
 						library = vim.api.nvim_get_runtime_file("", true),
 					},
-					diagnostics = {
-						globals = { "vim" },
-					},
+					diagnostics = { globals = { "vim" } },
 					telemetry = { enable = false },
 				},
 			},
 		})
-		vim.lsp.enable("lua_ls")
+
+		local servers = { "rust_analyzer", "clangd", "zls", "lua_ls", "ts_ls" }
+		for _, server in ipairs(servers) do
+			vim.lsp.enable(server)
+		end
 	end,
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,6 @@ return {
 	"williamboman/mason.nvim",
 	cmd = "Mason",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
@@ -15,14 +14,14 @@ return {
 				},
 			},
 		})
-
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "ts_ls", "zls" },
-			automatic_enable = false,
-		})
-
 		require("mason-tool-installer").setup({
-			ensure_installed = { "stylua", "prettier", "eslint_d", "jsonlint", "ast_grep" },
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"eslint_d",
+				"jsonlint",
+				"ast_grep",
+			},
 		})
 	end,
 }
